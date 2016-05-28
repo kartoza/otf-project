f3bcba8397f77496a4c32d9090305584de0663f8
Etienne Trimaille <etienne.trimaille@gmail.com>
2016-05-28 18:06:39 +0200

    metadata, remove UI

--- HelloServer.py
+++ HelloServer.py
@@ -32,8 +32,7 @@ try:
     from qgis.server import *
 except:
     pass
-from HelloServerDialog import HelloServerDialog
-import resources
+
 # Remote console filter auth defaults
 
 # The filter will issue a 403 Forbidden if the following
@@ -55,83 +54,6 @@ May need proper fcgid apache configuration:
 
 DEFAULT_USERID = 'test'
 DEFAULT_PASSWORD = 'qgis'
-
-class HelloServer:
-    """Test plugin for QGIS server"""
-
-    DEFAULT_PASSWORD = DEFAULT_PASSWORD
-    DEFAULT_USERID = DEFAULT_USERID
-    DEFAULT_REMOTE_ADDR = DEFAULT_REMOTE_ADDR
-
-    def __init__(self, iface):
-        # Save reference to the QGIS interface
-        self.iface = iface
-        self.canvas = iface.mapCanvas()
-
-    def initGui(self):
-        # Create action that will start plugin
-        self.action = QAction(QIcon(":/plugins/HelloServer/icons/icon.png"), "&HelloServer", self.iface.mainWindow())
-        # connect the action to the run method
-        QObject.connect(self.action, SIGNAL("activated()"), self.config)
-
-        # Add toolbar button and menu item
-        #self.iface.addPluginToMenu("HelloServer", self.action)
-        self.menu = QMenu("HelloServer")
-        self.menu.addAction(self.action)
-        self.menu.setIcon(QIcon(":/plugins/HelloServer/icons/icon.png"))
-        self.iface.pluginMenu().addMenu(self.menu)
-
-
-    # change settings
-    def config(self):
-        # create and show the dialog
-        dlg = HelloServerDialog()
-        # Get settings
-        settings = HelloServer.getSettings()
-        dlg.userid.setText(settings['userid'])
-        dlg.password.setText(settings['password'])
-        dlg.remote_addr.setText(settings['remote_addr'])
-        # show the dialog
-        dlg.show()
-        result = dlg.exec_()
-        # See if OK was pressed
-        if result == 1:
-            # save settings
-            HelloServer.storeSettings({
-                'userid':  dlg.userid.text(),
-                'password': dlg.password.text(),
-                'remote_addr': dlg.remote_addr.text(),
-            })
-
-
-    def unload(self):
-        # Remove the plugin menu item and icon
-        self.iface.removePluginMenu("HelloServer",self.action)
-
-
-    @staticmethod
-    def storeSettings(settings):
-        # Stores configuration in the project
-        proj = QgsProject.instance()
-        # store values
-        for k,v in settings.iteritems():
-            proj.writeEntry('hello_server_plugin', k, v)
-
-
-    @staticmethod
-    def getSettings():
-        # read values
-        proj = QgsProject.instance()
-        HelloServer.settings = {
-            'userid':  DEFAULT_USERID,
-            'password': DEFAULT_PASSWORD,
-            'remote_addr': DEFAULT_REMOTE_ADDR,
-        }
-        for k in ['userid', 'password', 'remote_addr']:
-            s = proj.readEntry("hello_server_plugin", k)[0]
-            if s:
-                HelloServer.settings[k] = s
-        return HelloServer.settings
 
 
 class HelloServerServer:
--- __init__.py
+++ __init__.py
@@ -3,10 +3,6 @@
  This script initializes the plugin, making it known to QGIS.
 """
 
-def classFactory(iface):
-    from HelloServer import HelloServer
-    return HelloServer(iface)
-
 
 def serverClassFactory(serverIface):
     from HelloServer import HelloServerServer
