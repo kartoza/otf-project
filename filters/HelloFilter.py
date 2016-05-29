5ab31374fd48cb5ea92e6bc0dfa246b25ca4bfe6
Etienne Trimaille <etienne.trimaille@gmail.com>
2016-05-29 17:13:17 +0200

    update

--- filters/HelloFilter.py
+++ filters/HelloFilter.py
@@ -18,27 +18,61 @@
 ***************************************************************************
 """
 
-from qgis.server import *
-from qgis.core import *
-from HelloServer.HelloServer import HelloServer
+
+from os.path import splitext, exists, isfile
+from qgis.server import QgsServerFilter
+from qgis.core import (
+    QgsProject,
+    QgsMapLayerRegistry,
+    QgsMessageLog,
+    QgsVectorLayer,
+    QgsRasterLayer)
+
 
 class HelloFilter(QgsServerFilter):
 
-    def __init__(self, serverIface):
-        super(HelloFilter, self).__init__(serverIface)
+    def __init__(self, server_iface):
+        super(HelloFilter, self).__init__(server_iface)
 
+    # noinspection PyPep8Naming
     def requestReady(self):
-        QgsMessageLog.logMessage("HelloFilter.requestReady")
-
-    def sendResponse(self):
-        QgsMessageLog.logMessage("HelloFilter.sendResponse")
-
-    def responseComplete(self):
-        QgsMessageLog.logMessage("HelloFilter.responseComplete")
         request = self.serverInterface().requestHandler()
         params = request.parameterMap()
-        if params.get('SERVICE', '').upper() == 'HELLO':
-            request.clearHeaders()
-            request.setHeader('Content-type', 'text/plain')
-            request.clearBody()
-            request.appendBody('HelloServer!')
+
+        map_file = params.get('MAP')
+
+        if not map_file.endswith('qgs'):
+
+            basename, _ = splitext(map_file)
+            project = basename + '.qgs'
+
+            if True:
+                QgsMessageLog.logMessage('Setting up project to %s' % project)
+                QgsProject.instance().setFileName(project)
+                QgsMessageLog.logMessage(
+                    'Project instance %s' % QgsProject.instance().fileName())
+
+                if map_file.endswith(('shp', 'geojson')):
+                    layer = QgsVectorLayer(map_file, 'layer', 'ogr')
+                elif map_file.endswith(('asc', 'tiff', 'tif')):
+                    layer = QgsRasterLayer(map_file, 'layer')
+                else:
+                    QgsMessageLog.logMessage('Invalid format : %s' % map_file)
+                    return
+
+                QgsMessageLog.logMessage(
+                    'Layer is valid : %s' % layer.isValid())
+
+                # Add layer to the registry
+                QgsMapLayerRegistry.instance().addMapLayer(layer)
+                QgsProject.instance().write()
+
+                if not exists(project) and not isfile(project):
+                    QgsMessageLog.logMessage(QgsProject.instance().error())
+                    return
+
+            del params['MAP']
+            request.setParameter('MAP', project)
+
+        else:
+            QgsMessageLog.logMessage('QGIS Project existing.')
